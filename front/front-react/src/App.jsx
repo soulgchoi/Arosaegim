c100080e9ed9702c3d4fe05e193c56c371df19b1
양찬우 <oizys18@gmail.com>
2020-05-19 20:50:22 +0900

    Add write page

--- front/front-react/src/App.jsx
+++ front/front-react/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Storage } from './storage/Storage'
-import Main from "./components/main/Main";
+import Write from "./components/write/Write";
+// import Main from "./components/main/Main";
 import TopBar from './components/common/menu/TopBar';
 import SideMenu from './components/common/menu/SideMenu';
 import Auth from "./components/auth/Auth";
@@ -27,7 +28,8 @@ class App extends Component {
       <Storage.Provider value={this.state}>
         <Router>
           <Route path="/" component={TopBar}/>
-          <Route path="/" component={Main} />
+          <Route path="/write" component={Write} />
+          {/* <Route path="/" component={Main} /> */}
           <Route path="/" component={SideMenu}/>
           <Route path="/auth" component={Auth} />
         </Router>
